573cbf0da8b129970e543c91be7a29825308b9eb
Ellis Valentiner <ellisvalentiner@gmail.com>
2017-08-29 11:10:06 -0400

    Add is.weekend function Update NAMESPACE exports Update R Documentation

--- R/instants.R
+++ R/instants.R
@@ -27,3 +27,18 @@ yesterday <- function(tzone = "") {
 tomorrow <- function(tzone = "") {
   as_date(now(tzone) + days(1))
 }
+
+#' Is x a weekend?
+#'
+#' @export is.weekend
+#' @param x a POSIXct, POSIXlt, Date, chron, yearmon, yearqtr, zoo, zooreg,
+#'   timeDate, xts, its, ti, jul, timeSeries, or fts object.
+#' @return boolean indicating whether x is a weekend
+#'
+#' @examples
+#' is.weekend("2017-08-29")  # FALSE
+#' is.weekend("2017-09-02")  # TRUE
+is.weekend <- function(x) {
+  wday(x = as_date(x), label = TRUE, abbr = FALSE) %in% c("Saturday", "Sunday")
+}
+
