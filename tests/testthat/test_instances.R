3a305dc41b17d75a7f50e96b3c5713b85953d24b
Ellis Valentiner <ellisvalentiner@gmail.com>
2018-10-25 14:42:21 -0400

    Run stylr

--- tests/testthat/test_instances.R
+++ tests/testthat/test_instances.R
@@ -68,8 +68,8 @@ test_that("hours", {
   expect_equal(as_date(hours_ago(24)), days_ago(1))
   expect_equal(as_date(hours_hence(24)), days_hence(1))
 
-  expect_equal(as_date(hours_ago(7*24)), weeks_ago(1))
-  expect_equal(as_date(hours_hence(7*24)), weeks_hence(1))
+  expect_equal(as_date(hours_ago(7 * 24)), weeks_ago(1))
+  expect_equal(as_date(hours_hence(7 * 24)), weeks_hence(1))
 })
 
 test_that("minutes", {
@@ -77,15 +77,15 @@ test_that("minutes", {
   expect_equal(minute(minutes_ago(-1)), minute(minutes_hence(1)))
 
   expect_equal(hour(minutes_ago(60)), hour(hours_ago(1)))
-  expect_equal(day(minutes_ago(60*24)), day(days_ago(1)))
-  expect_equal(day(minutes_ago(60*24*7)), day(weeks_ago(1)))
+  expect_equal(day(minutes_ago(60 * 24)), day(days_ago(1)))
+  expect_equal(day(minutes_ago(60 * 24 * 7)), day(weeks_ago(1)))
 })
 
 test_that("seconds", {
   expect_equal(minute(seconds_ago(60)), minute(seconds_hence(-60)))
   expect_equal(minute(seconds_ago(60)), minute(minutes_ago(1)))
-  expect_equal(hour(seconds_ago(60*60)), hour(hours_ago(1)))
-  expect_equal(day(seconds_ago(24*60*60)), day(days_ago(1)))
+  expect_equal(hour(seconds_ago(60 * 60)), hour(hours_ago(1)))
+  expect_equal(day(seconds_ago(24 * 60 * 60)), day(days_ago(1)))
 })
 
 test_that("this_month", {
